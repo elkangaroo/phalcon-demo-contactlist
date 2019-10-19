f62d9735b99561169e9e4cc93707a973a9da0056
Alexander Wenzel <elkangaroo@gmail.com>
2019-10-20 01:47:10 +0200

    Bootstrap phalcon framework; Hello IndexController

--- .htrouter.php
+++ .htrouter.php
@@ -2,7 +2,7 @@
 
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
-  return false;
+    return false;
 }
 
 $_GET['_url'] = $_SERVER['REQUEST_URI'];
--- public/index.php
+++ public/index.php
@@ -1,4 +1,34 @@
 <?php
 
-echo "Hello phalcon";
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Loader;
+use Phalcon\Mvc\Application;
+use Phalcon\Mvc\View;
+use Phalcon\Url as UrlProvider;
+
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
+
+$loader = new Loader();
+$loader->registerDirs([
+    APP_PATH . '/controllers/',
+    APP_PATH . '/models/',
+]);
+$loader->register();
+
+$di = new FactoryDefault();
+$di->set('view', function () {
+    $view = new View();
+    $view->setViewsDir(APP_PATH . '/views/');
+    return $view;
+});
+$di->set('url', function () {
+    $url = new UrlProvider();
+    $url->setBaseUri('/');
+    return $url;
+});
+
+$application = new Application($di);
+$response = $application->handle($_SERVER["REQUEST_URI"]);
+$response->send();
 
